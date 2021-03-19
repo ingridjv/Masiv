815d738439c994199dfa1c78351e80055acd1266
ingridjv <ingrid_flak17@hotmail.com>
2021-03-18 20:46:58 -0500

    Consulta random

---
+++ comic/src/components/comic.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="hello">
+  
+    <h2>{{obj.title}}</h2>
+    <div id="image" v-bind:style="{ backgroundImage:'url('+obj.url+')' }"></div>
+  </div>
+</template>
+
+<script>
+import axios from 'axios'
+
+export default {
+  name: 'comic',
+  
+data() {   
+      return {
+         dataComic: [],
+          obj: []
+      }
+    },
+
+    methods: {
+     
+    },
+
+    mounted(){  
+     let vueHome = this;      
+      
+      axios.get("https://jsonplaceholder.typicode.com/photos?albumId=80")
+      .then(function (response){
+        vueHome.dataComic = response.data;        
+        const idx = Math.floor(Math.random() * vueHome.dataComic.length);
+        console.log('ramdom');
+        console.log(vueHome.dataComic[idx]);
+        
+         vueHome.obj= vueHome.dataComic[idx]
+      })
+    },
+    
+    
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="less">
+
+</style>
